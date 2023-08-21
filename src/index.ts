01368c249a84a24b57cd9272d18a09addef19a31
yarqui <108986486+yarqui@users.noreply.github.com>
2023-08-21 17:12:53 +0300

    add an example code

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,22 @@ function isDog(pet: Dog | Fish): pet is Dog {
   return "bark" in pet;
 }
 
+let Doggy: Dog = {
+  legs: 4,
+  bark: () => {
+    console.log("bark");
+  },
+};
+
+let Fishy: Fish = {
+  fins: 2,
+  swim: () => {
+    console.log("swoosh");
+  },
+};
+
+pet = Fishy;
+
 // Перевіряємо, чи є наш вихованець собакою перед тим, як використовувати метод bark
 if (isDog(pet)) {
   pet.bark(); // OK, тепер TypeScript знає, що pet - це Dog
