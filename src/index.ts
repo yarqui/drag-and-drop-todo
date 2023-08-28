fd24854c252dbdfa9ef0e8273c3fde1e167f2388
Yaroslav Pelykh <y.pelykh@gmail.com>
2023-08-28 19:32:37 +0300

    lesson 65

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 class Department {
-  // private name: string;
   private employees: string[] = [];
 
   constructor(private id: string, public name: string) {}
@@ -12,13 +11,41 @@ class Department {
     this.employees.push(employee);
   }
 
-  getEmployeeInfo() {
+  getEmployeeInfo(): void {
     console.log(this.employees.length);
     console.log(this.employees);
   }
 }
 
-const sales = new Department("id1", "Sales");
-sales.describe();
+class ITDepartment extends Department {
+  constructor(id: string, public admins: string[]) {
+    super(id, "IT");
+  }
+}
 
-const salesCopy = { id: "ds2", name: "Yar", describe: sales.describe };
+const it = new ITDepartment("id1", ["Yar"]);
+it.addEmployee("Mauritius");
+it.addEmployee("Valentine");
+it.describe();
+console.log("it:", it);
+
+class Accounting extends Department {
+  private reports: string[] = [];
+
+  constructor(id: string) {
+    super(id, "AccountingDep");
+  }
+
+  addReport = (report: string) => {
+    this.reports.push(report);
+  };
+
+  printReport() {
+    console.log(this.reports);
+  }
+}
+
+const accounting = new Accounting("iddee");
+accounting.printReport();
+accounting.addReport("This is the report that we have to do");
+accounting.printReport();
