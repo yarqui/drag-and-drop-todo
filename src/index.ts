6d6c2d814f0f72f632ef2821a65cc9676cd75c08
Yaroslav Pelykh <y.pelykh@gmail.com>
2023-09-03 12:29:52 +0300

    lesson 77

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,36 @@
-interface Named {
-  readonly name: string;
+// interface Named {
+//   readonly name: string;
+// }
+
+// interface Greetable extends Named {
+//   greet(phrase: string): void;
+// }
+
+// class Person implements Greetable {
+//   name: string;
+
+//   constructor(n: string, public age: number) {
+//     this.name = n;
+//   }
+
+//   greet(phrase: string): void {
+//     console.log(`${phrase} ${this.name}. Your age is: ${this.age}`);
+//   }
+// }
+
+// let user1: Greetable;
+
+// user1 = new Person("Yar", 37);
+
+// you also can use types for it
+// type AddFn = (a: number, b: number) => number;
+interface AddFn {
+  (a: number, b: number): number;
 }
 
-interface Greetable extends Named {
-  greet(phrase: string): void;
-}
+let add: AddFn = (a, b) => {
+  console.log(a + b);
+  return a + b;
+};
 
-class Person implements Greetable {
-  name: string;
-
-  constructor(n: string, public age: number) {
-    this.name = n;
-  }
-
-  greet(phrase: string): void {
-    console.log(`${phrase} ${this.name}. Your age is: ${this.age}`);
-  }
-}
-
-let user1: Greetable;
-
-user1 = new Person("Yar", 37);
+add(5, 6);
