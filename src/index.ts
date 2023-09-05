29d01ff12b58cbf62b974ac9b2a41810224e4c4a
Yaroslav Pelykh <y.pelykh@gmail.com>
2023-09-05 21:33:01 +0300

    lesson 86 Type Casting

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,5 @@
-interface Fish {
-  type: "fish";
-  swimmingSpeed: number;
+const input = document.getElementById("message");
+
+if (input) {
+  (input as HTMLInputElement).value = "Hey there!";
 }
-interface Bird {
-  type: "bird";
-  flyingSpeed: number;
-}
-
-type Animal = Fish | Bird;
-
-const checkAnimalSpeed = (animal: Animal) => {
-  let speed: number;
-
-  switch (animal.type) {
-    case "fish":
-      speed = animal.swimmingSpeed;
-      break;
-    case "bird":
-      speed = animal.flyingSpeed;
-  }
-
-  console.log(`The animal moves with the speed: ${speed} km/h`);
-};
-
-checkAnimalSpeed({ type: "bird", flyingSpeed: 6 });
