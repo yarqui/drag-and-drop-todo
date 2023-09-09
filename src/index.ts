0815a6f268f03292544032c74dd0537d53f1336a
Yaroslav Pelykh <y.pelykh@gmail.com>
2023-09-09 15:58:07 +0300

    lesson 87 - index properties

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
-const input = document.getElementById("message");
-
-if (input) {
-  (input as HTMLInputElement).value = "Hey there!";
+interface ErrorContainer {
+  id: string; // 📍 we can also predefine some of the properties WITH the same type
+  [prop: string]: string;
 }
+
+const errorWrap: ErrorContainer = {
+  id: "12", // 📍 we could type 1: "12" - 1 can be converted to string, because the type of key is a string
+  email: "Wrong Email",
+};
