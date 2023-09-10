6bf82a689cb25bdb4913a845b9a6e73370e12a08
Yaroslav Pelykh <y.pelykh@gmail.com>
2023-09-10 12:44:29 +0300

    lesson 88 function overload

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
-interface ErrorContainer {
-  id: string; // 📍 we can also predefine some of the properties WITH the same type
-  [prop: string]: string;
+type Combinable = number | string;
+
+type Overload = {
+  (a: number, b: number): number;
+  (a: string, b: string): string;
+  (a: number, b: string): string;
+  (a: string, b: number): string;
+};
+
+function add(a: number, b: number): number;
+function add(a: string, b: string): string;
+function add(a: number, b: string): string;
+function add(a: string, b: number): string;
+
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
 }
 
-const errorWrap: ErrorContainer = {
-  id: "12", // 📍 we could type 1: "12" - 1 can be converted to string, because the type of key is a string
-  email: "Wrong Email",
-};
+add(1, 2);
