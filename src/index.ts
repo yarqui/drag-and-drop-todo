f9b527a647da5b1cafaaaf5c94089a1a7c34d4f6
yarqui <y.pelykh@gmail.com>
2024-02-07 21:51:43 -0500

    lesson 108

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,23 @@
-// We can create a decorator factory for more flexibility.
-// Create a function that returns a decorator function
+// Factories run as the regular JS code: top to bottom, but the decorator functions run bottom to top.
+
 function Logger(logString: string): Function {
+  console.log("LOGGER FACTORY"); // ❗1st
+
   return function (constructor: Function): void {
-    console.log(logString);
-    console.log("constructor", constructor);
+    console.log(logString); // ❗5th
+    console.log("constructor", constructor); // ❗6th
   };
 }
 
 function WithTemplate(template: string, elRef: string): Function {
+  console.log("TEMPLATE FACTORY"); // ❗2nd
+
   return function (constructor: any): void {
+    console.log("Rendering template"); // ❗3rd
+
     const box = document.getElementById(elRef);
     const person = new constructor();
+
     if (box) {
       box.innerHTML = template;
       box.querySelector("h1")!.textContent = person.name;
@@ -18,15 +25,12 @@ function WithTemplate(template: string, elRef: string): Function {
   };
 }
 
-// Here we have to call this factory and pass the argument
-// @Logger("LOGGING - PERSON...")
+@Logger("LOGGING - PERSON...")
 @WithTemplate("<h1></h1>", "app")
 class Person {
   public name: string = "Yar";
 
   constructor() {
-    console.log(`Creating a person... ${this.name}`);
+    console.log(`Creating a person...`); //❗ 4th
   }
 }
-
-const yar = new Person();
