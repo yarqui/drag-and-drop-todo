b9af3b65843d378d609c29007fdd7ad2568fdcb4
yarqui <y.pelykh@gmail.com>
2024-02-07 20:54:33 -0500

    lesson 106.1

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
-// A decorator is just a function.
-// Decorators should have arguments and are executed when the class is defined, not instantiated
-function Logger(constructor: Function): void {
-  console.log("Logging...");
-  console.log("constructor", constructor);
+// We can create a decorator factory for more flexibility.
+// Create a function that returns a decorator function
+function Logger(logString: string): Function {
+  return function (constructor: Function): void {
+    console.log(logString);
+    console.log("constructor", constructor);
+  };
 }
 
-// To add a decorator we use @ symbol and right after it we should point to a function.
-@Logger
+// Here we have to call this factory and pass the argument
+@Logger("LOGGING - PERSON...")
 class Person {
   constructor(public name: string) {
     console.log(`Creating a person... ${name}`);
   }
 }
 
-// const yar = new Person("Yar");
+const yar = new Person("Yar");
