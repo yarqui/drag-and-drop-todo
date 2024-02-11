a228c62ec26fa51dba328a7b9f6f06359928604e
yarqui <y.pelykh@gmail.com>
2024-02-11 11:47:01 -0500

    lesson 112. Returning (and changing) a Class in a Class Decorator

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-// // Factories run as the regular JS code: top to bottom, but the decorator functions run bottom to top.
-
 // function Logger(logString: string): Function {
 //   console.log("LOGGER FACTORY"); // ❗1st
 
@@ -9,100 +7,109 @@
 //   };
 // }
 
-// function WithTemplate(template: string, elRef: string): Function {
-//   console.log("TEMPLATE FACTORY"); // ❗2nd
-
-//   return function (constructor: any): void {
-//     console.log("Rendering template"); // ❗3rd
-
-//     const box = document.getElementById(elRef);
-//     const person = new constructor();
-
-//     if (box) {
-//       box.innerHTML = template;
-//       box.querySelector("h1")!.textContent = person.name;
-//     }
-//   };
+// function LogProperty(target: any, propertyName: string | Symbol) {
+//   console.log("Property decorator----------------------------");
+//   console.log("target: ", target);
+//   console.log("propertyName: ", propertyName);
 // }
 
-// // @Logger("LOGGING - PERSON...")
-// @WithTemplate("<h1></h1>", "app")
-// class Person {
-//   public name: string = "Yar";
+// function LogAccessor(
+//   target: any,
+//   name: string,
+//   descriptor: PropertyDescriptor
+// ) {
+//   console.log("Accessor decorator----------------------------");
+//   console.log("target: ", target);
+//   console.log("name: ", name);
+//   console.log("descriptor: ", descriptor);
+// }
 
-//   constructor() {
-//     console.log(`Creating a person...`); //❗ 4th
+// // Method decorator is the same as a the Accessor decorator
+// function LogMethod(
+//   target: any,
+//   name: string | Symbol,
+//   descriptor: PropertyDescriptor
+// ) {
+//   console.log("Method decorator----------------------------");
+//   console.log("target: ", target);
+//   console.log("name: ", name);
+//   console.log("descriptor: ", descriptor);
+// }
+
+// function LogParameter(
+//   target: any,
+//   methodName: string | Symbol,
+//   position: number
+// ) {
+//   console.log("Parameter decorator----------------------------");
+//   console.log("target: ", target);
+//   console.log("methodName: ", methodName);
+//   console.log("position: ", position);
+// }
+
+// class Product {
+//   // @LogProperty
+//   title: string;
+//   private _price: number;
+
+//   constructor(title: string, price: number) {
+//     this.title = title;
+//     this._price = price;
+//   }
+
+//   // @LogAccessor
+//   set price(val: number) {
+//     if (val > 0) {
+//       this._price = val;
+//     }
+//   }
+
+//   get price() {
+//     return this._price;
+//   }
+
+//   // @LogMethod
+//   getPriceWithTax(@LogParameter tax: number) {
+//     return this._price * (1 + tax);
 //   }
 // }
 
-function LogProperty(target: any, propertyName: string | Symbol) {
-  console.log("Property decorator----------------------------");
-  console.log("target: ", target);
-  console.log("propertyName: ", propertyName);
+function WithTemplate(template: string, elRef: string) {
+  console.log("TEMPLATE FACTORY");
+
+  // ❗specify that type T extends the object with  the "new" property (meaning that it will be a constructor, and can be instantiated). This constructor will accept any number of arguments, and this constructor will return an object with the property "name" to make sure that there will be such a property in the object.
+  return function <T extends { new (...args: any[]): { name: string } }>(
+    // The decorator function receives an original constructor of a class, that we added decorator to
+    originalConstructor: T
+  ) {
+    // In a decorator function we return a new constructor, which replaces the original constructor (original class).
+    // We also extend from the originalConstructor as we don't want to loose the properties of the originalConstructor
+    return class extends originalConstructor {
+      // pass to the new constructor any number of arguments
+      constructor(..._: any[]) {
+        super();
+
+        // add our own new logic to the original class, therefore it will be run only when our class is instantiated
+        console.log("Rendering template");
+        const box = document.getElementById(elRef);
+
+        if (box) {
+          box.innerHTML = template;
+          box.querySelector("h1")!.textContent = this.name; // add "this" to reference the instance of the class
+        }
+      }
+    };
+  };
 }
 
-function LogAccessor(
-  target: any,
-  name: string,
-  descriptor: PropertyDescriptor
-) {
-  console.log("Accessor decorator----------------------------");
-  console.log("target: ", target);
-  console.log("name: ", name);
-  console.log("descriptor: ", descriptor);
-}
+// @Logger("LOGGING - PERSON...")
+@WithTemplate("<h1></h1>", "#app")
+class Person {
+  public name: string = "Yar";
 
-// Method decorator is the same as a the Accessor decorator
-function LogMethod(
-  target: any,
-  name: string | Symbol,
-  descriptor: PropertyDescriptor
-) {
-  console.log("Method decorator----------------------------");
-  console.log("target: ", target);
-  console.log("name: ", name);
-  console.log("descriptor: ", descriptor);
-}
-
-function LogParameter(
-  target: any,
-  methodName: string | Symbol,
-  position: number
-) {
-  console.log("Parameter decorator----------------------------");
-  console.log("target: ", target);
-  console.log("methodName: ", methodName);
-  console.log("position: ", position);
-}
-
-class Product {
-  @LogProperty
-  title: string;
-  private _price: number;
-
-  constructor(title: string, price: number) {
-    this.title = title;
-    this._price = price;
-  }
-
-  @LogAccessor
-  set price(val: number) {
-    if (val > 0) {
-      this._price = val;
-    }
-  }
-
-  get price() {
-    return this._price;
-  }
-
-  @LogMethod
-  getPriceWithTax(@LogParameter tax: number) {
-    return this._price * (1 + tax);
+  constructor() {
+    console.log(`Creating a person...`);
   }
 }
 
-// const p = new Product("Mercedes", 10);
-// console.log(p.getPriceWithTax(10));
-// console.log((p.price = 5));
-// console.log(p.getPriceWithTax(10));
+const p = new Person();
