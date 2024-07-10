b2e3be7937303c53110bb4d64e6e43797cbb8b8a
yarqui <y.pelykh@gmail.com>
2024-07-10 17:42:22 -0400

    edit tsconfig & app

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,24 @@
+// Autobind decorator
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+
+// ProjectInput class
 class ProjectInput {
   templateEl: HTMLTemplateElement;
   rootEl: HTMLDivElement;
-  element: HTMLFormElement;
+  formEl: HTMLFormElement;
+  titleInputEl: HTMLInputElement;
+  descriptionInputEl: HTMLInputElement;
+  peopleInputEl: HTMLInputElement;
 
   constructor() {
     this.templateEl = <HTMLTemplateElement>(
@@ -10,12 +27,46 @@ class ProjectInput {
     this.rootEl = <HTMLDivElement>document.getElementById("app");
 
     const importedNode = document.importNode(this.templateEl.content, true);
-    this.element = <HTMLFormElement>importedNode.firstElementChild;
+    this.formEl = <HTMLFormElement>importedNode.firstElementChild;
+    this.formEl.id = "user-input";
+
+    this.titleInputEl = <HTMLInputElement>this.formEl.querySelector("#title");
+    this.descriptionInputEl = <HTMLInputElement>(
+      this.formEl.querySelector("#description")
+    );
+    this.peopleInputEl = <HTMLInputElement>this.formEl.querySelector("#people");
+
+    this.configure();
     this.attach();
   }
 
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputEl.value;
+    const enteredDescription = this.descriptionInputEl.value;
+    const enteredPeople = this.peopleInputEl.value;
+
+    return [enteredTitle, enteredDescription, Number(enteredPeople)];
+  }
+
+  @autobind
+  private submitHandler(e: Event) {
+    e.preventDefault();
+    console.log(this.titleInputEl.value);
+    const userInput = this.gatherUserInput();
+
+    if (Array.isArray(userInput)) {
+      const [title, desc, people] = userInput;
+      console.log(title, desc, people);
+      this.formEchl.reset();
+    }
+  }
+
+  private configure() {
+    this.formEl.addEventListener("submit", this.submitHandler);
+  }
+
   private attach() {
-    this.rootEl.insertAdjacentElement("afterbegin", this.element);
+    this.rootEl.insertAdjacentElement("afterbegin", this.formEl);
   }
 }
 
