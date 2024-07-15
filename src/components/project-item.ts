7d3ad3287bc92540c8b038d596eecf0f9a33c6b3
yarqui <y.pelykh@gmail.com>
2024-07-15 14:58:35 -0400

    add webpack dev workflow

--- src/components/project-item.ts
+++ src/components/project-item.ts
@@ -1,7 +1,7 @@
-import { Project } from "../models/project.js";
-import { Draggable } from "../models/drag-drop.js";
-import { Component } from "./base-component.js";
-import { autobind } from "../decorators/autobind.js";
+import { Project } from "../models/project";
+import { Draggable } from "../models/drag-drop";
+import { Component } from "./base-component";
+import { autobind } from "../decorators/autobind";
 
 // Project Item class
 export class ProjectItem
